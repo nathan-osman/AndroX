2317d0382a3dccce30090dfbfb760b3f3b062500
Nathan Osman <admin@quickmediasolutions.com>
2013-04-03 14:40:37 -0700

    Added some more stuff to the OpenSSH patch.

--- src/openssh.mk
+++ src/openssh.mk
@@ -4,18 +4,18 @@ PACKAGE = openssh
 
 $(PACKAGE)_NAME         := OpenSSH
 $(PACKAGE)_VERSION      := 6.2p1
-$(PACKAGE)_DEPENDENCIES := openssl
+#$(PACKAGE)_REQUIREMENTS := autoconf
+$(PACKAGE)_DEPENDENCIES := openssl ldns
 $(PACKAGE)_PATH         := http://ftp.openbsd.org/pub/OpenBSD/OpenSSH/portable
 $(PACKAGE)_MD5          := 7b2d9dd75b5cf267ea1737ec75500316
 
 define configure-$(PACKAGE)
-	cat $(SRC_DIR)/patch/$(PACKAGE).patch | patch -p0; \
+	patch -p0 <'$(SRC_DIR)/patch/$(PACKAGE).patch'; \
 	autoconf; \
 	./configure --prefix='$(INSTALL_DIR)' \
 		    --host=$(HOST) \
-		    --with-cflags=-Dfd_mask=int \
+		    --with-ldns='$(INSTALL_DIR)' \
 		    --with-ssl-dir='$(INSTALL_DIR)' \
-		    ac_cv_search_getrrsetbyname=yes \
 		    ac_cv_header_sys_un_h=yes
 endef
 
