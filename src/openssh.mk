f2273a844c2102cc60dcafeb338efcecc8bbd3d3
Nathan Osman <admin@quickmediasolutions.com>
2013-04-03 10:54:25 -0700

    Added initial Makefile for OpenSSH, which configures but does NOT build at this point.

---
+++ src/openssh.mk
@@ -0,0 +1,19 @@
+# Builds OpenSSH for Android.
+
+PACKAGE = openssh
+
+$(PACKAGE)_NAME         := OpenSSH
+$(PACKAGE)_VERSION      := 6.2p1
+$(PACKAGE)_DEPENDENCIES := openssl
+$(PACKAGE)_PATH         := http://ftp.openbsd.org/pub/OpenBSD/OpenSSH/portable
+$(PACKAGE)_MD5          := 7b2d9dd75b5cf267ea1737ec75500316
+
+define configure-$(PACKAGE)
+	./configure --prefix='$(INSTALL_DIR)' \
+		    --host=$(HOST) \
+		    --with-ssl-dir='$(INSTALL_DIR)'
+endef
+
+define build-$(PACKAGE)
+	make install
+endef
