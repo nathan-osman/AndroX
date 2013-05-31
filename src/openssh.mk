af24a0fc5207e59bfb3e0793cf8b8095f9b16ab3
Nathan Osman <admin@quickmediasolutions.com>
2013-05-30 23:35:26 -0700

    Updated OpenSSH patch to match upstream.

--- src/openssh.mk
+++ src/openssh.mk
@@ -9,8 +9,17 @@ $(PACKAGE)_DEPENDENCIES := openssl ldns
 $(PACKAGE)_PATH         := http://ftp.openbsd.org/pub/OpenBSD/OpenSSH/portable
 $(PACKAGE)_MD5          := 7b2d9dd75b5cf267ea1737ec75500316
 
+# I have submitted a total of three patches to the OpenSSH portability team to
+# allow OpenSSH to compile for Android.
+#
+# https://bugzilla.mindrot.org/show_bug.cgi?id=2085
+# https://bugzilla.mindrot.org/show_bug.cgi?id=2086
+# https://bugzilla.mindrot.org/show_bug.cgi?id=2087
+#
+# Each of these is set for inclusion (in some form) in the next release.
+
 define configure-$(PACKAGE)
-	patch -p0 <'$(SRC_DIR)/patch/$(PACKAGE).patch'; \
+	patch -p0 <'$(SRC_DIR)/patch/$(PACKAGE)-upstream.patch'; \
 	autoconf; \
 	./configure --prefix='$(INSTALL_DIR)' \
 		    --host=$(HOST) \
